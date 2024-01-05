8ce2b6b959ed98470d7a3580da608ccafb423200
Martin <Martin.Andre.Jarneid@ssn.no>
2024-01-05 09:22:02 +0100

    Move border from container to chat frame

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 const Chat = () => {
     const [showChat, setShowChat] = useState(true);
     return (
-        <div className="fixed bottom-2 right-2 border dark:border-none">
+        <div className="fixed bottom-2 right-2">
             <div className={!showChat ? "hidden" : ""}>
                 <iframe
-                    className={"h-[500px] w-96"}
+                    className="h-[500px] w-96 border dark:border-none"
                     src="https://copilotstudio.microsoft.com/environments/Default-45b10503-08a5-4e5b-84f3-61fcf02ca92c/bots/cr513_chatbotSsn/webchat?__version__=2"
                 ></iframe>
                 <span
