ee12970498d8a1e68ccb401d9f3c18e512fa5829
Martin <Martin.Andre.Jarneid@ssn.no>
2024-01-05 09:17:20 +0100

    Set position to fixed, and hide instead of remove on minimize

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -3,23 +3,22 @@ import { useState } from "react";
 const Chat = () => {
     const [showChat, setShowChat] = useState(true);
     return (
-        <div className="absolute bottom-2 right-2 border dark:border-none">
-            {showChat ? (
-                <>
-                    <iframe
-                        className="h-[500px] w-96 "
-                        src="https://copilotstudio.microsoft.com/environments/Default-45b10503-08a5-4e5b-84f3-61fcf02ca92c/bots/cr513_chatbotSsn/webchat?__version__=2"
-                    ></iframe>
-                    <span
-                        className="absolute -top-9 right-2 text-8xl cursor-pointer"
-                        onClick={() => {
-                            setShowChat(false);
-                        }}
-                    >
-                        -
-                    </span>
-                </>
-            ) : (
+        <div className="fixed bottom-2 right-2 border dark:border-none">
+            <div className={!showChat ? "hidden" : ""}>
+                <iframe
+                    className={"h-[500px] w-96"}
+                    src="https://copilotstudio.microsoft.com/environments/Default-45b10503-08a5-4e5b-84f3-61fcf02ca92c/bots/cr513_chatbotSsn/webchat?__version__=2"
+                ></iframe>
+                <span
+                    className="absolute -top-9 right-2 text-8xl cursor-pointer"
+                    onClick={() => {
+                        setShowChat(false);
+                    }}
+                >
+                    -
+                </span>
+            </div>
+            {!showChat && (
                 <button
                     className="btn-circle btn h-20 w-20 btn-accent flex items-center"
                     onClick={() => {
